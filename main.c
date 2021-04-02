b21c5389626e40993d7e39ca2235f4c4d1c8023e
Maximilinao <maximiliano.pan@hotmail.com>
2021-04-02 19:17:00 -0300

    added main.c

---
+++ main.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+/**
+ * main - exacute the interactive or non-interactive mode of the shell.
+ * @ac: argument count.
+ * @av: argument vector.
+ */
+int main(int ac, char **av, char **env)
+{
+	size_t len = 0;
+	char *stdin_buffer = NULL;
+	char *input_buffer = NULL;
+
+	if (!isatty(0))
+	{
+		/* reads stdin if arguments are passed with pipeline */
+		getline(&stdin_buffer, &len, stdin);
+		input_buffer = strtok(stdin_buffer, "\n");
+		return (only_execute(input_buffer));
+	}
+	/* strat interactive shell */
+	return (start_shell(env));
+}
