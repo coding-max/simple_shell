05419e39f96d6a37ad14aff1d7d747b5ac196854
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-04-04 09:54:57 -0300

    don't rm '\n' from buffer before call only_execute

--- main.c
+++ main.c
@@ -12,14 +12,14 @@ int main(int ac, char **av, char **env)
 {
 	size_t len = 0;
 	char *stdin_buffer = NULL;
-	char *input_buffer = NULL;
+	// char *input_buffer = NULL;
 
 	if (!isatty(0))
 	{
 		/* reads stdin if arguments are passed with pipeline */
 		getline(&stdin_buffer, &len, stdin);
-		input_buffer = strtok(stdin_buffer, "\n");
-		return (only_execute(input_buffer));
+		// input_buffer = strtok(stdin_buffer, "\n");
+		return (only_execute(stdin_buffer));
 	}
 	/* strat interactive shell */
 	return (start_shell(env));
