bf75c78054bd907258820e81f266c2999bdb9db4
Maximiliano Pan <maximiliano.pan@hotmail.com>
2021-04-04 10:04:17 -0300

    don't rm '\n' from buffer before call only_execute

--- main.c
+++ main.c
@@ -1,6 +1,5 @@
 #include "shell.h"
 
-// TODO update flowchart
 /**
  * main - exacute the interactive or non-interactive mode of the shell.
  * @ac: argument count.
@@ -12,13 +11,11 @@ int main(int ac, char **av, char **env)
 {
 	size_t len = 0;
 	char *stdin_buffer = NULL;
-	// char *input_buffer = NULL;
 
 	if (!isatty(0))
 	{
 		/* reads stdin if arguments are passed with pipeline */
 		getline(&stdin_buffer, &len, stdin);
-		// input_buffer = strtok(stdin_buffer, "\n");
 		return (only_execute(stdin_buffer));
 	}
 	/* strat interactive shell */
